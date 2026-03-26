a3df919a20fb7daf8a6c6b8fd11e6c666c2c7cb0
VishalPDSingh <vishalprasadsingh52@gmail.com>
2026-03-26 23:42:10 +0530

    first commit

--- Basics/UserInput.java
+++ Basics/UserInput.java
@@ -1,12 +1,11 @@
-package intro_Of_Lang;
+package Basics;
 
 import java.util.Scanner;
 
-public class Input_OutPut {
+public class UserInput {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(n);
-        sc.close();
     }
 }
